e77d3f9e76520c83622ccf8adaf551704327dba7
afolabiopakunle <opakunleafolabi@gmail.com>
2021-09-24 08:22:24 +0100

    all working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AwayComponent } from './away/away.component';
+import { GamesComponent } from './games/games.component';
 
 const routes: Routes = [
-  {}
+  {path: '', component: HomeComponent},
+  {path: 'away', component: AwayComponent},
+  {path: 'games/:id', component: GamesComponent}
 ]
 @NgModule({
   imports: [
---
+++ src/app/games/games.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-games',
+  templateUrl: './games.component.html',
+  styleUrls: ['./games.component.css']
+})
+export class GamesComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { 
+    this.route.params.subscribe(data => {
+      console.log(data.id)
+    })
+  }
+      
+  ngOnInit() {
+
+  }
+
+}
